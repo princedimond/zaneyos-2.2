25a1993a5fec62fc32aff1f1a817d47709e6c5bf
princedimond <princedimond@gmail.com>
2024-06-23 08:40:21 -0500

    nitial commit for zaneyos2.1

---
+++ hosts/5CG84633PQ/variables.nix
@@ -0,0 +1,15 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "princedimond";
+  gitEmail = "princedimond@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "microsoft-edge"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "warp-terminal"; # Set Default System Terminal
+}
